6ef77040eea88282a4fc3f397bdb403d9828c6b8
Rose <mateodevelop@outlook.com>
2025-10-21 05:22:09 -0300

    primer commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+function Navbar () {
+  return (
+    <nav>
+      <h1>Tienda360</h1>
+      <ul>
+        <li>Keyboards</li>
+        <li>Mouses</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
